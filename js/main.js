27dc1665723f8009d9374631a46702946c1cd9f2
Michael Zeitler <m.zeitler1987@gmail.com>
2026-05-24 11:21:01 +0200

    Implement function for updating active nav links

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
-document.addEventListener('DOMContentLoaded', init());
+document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     console.log('initializing');
-    
+    updateActiveNavLinks();
+}
+
+function updateActiveNavLinks() {
+    let navLinks = document.querySelectorAll('.nav-list a');
+
+    navLinks.forEach((link) => {
+        link.addEventListener('click', () => {
+            resetActive(navLinks);
+            setActive(link);
+        });
+    });
+}
+
+function resetActive(links) {
+    links.forEach((link) => {
+        link.classList.remove('active');
+    });
+}
+
+function setActive(link) {
+    link.classList.add('active');
 }
