87cedd577d916fdc13fceb80915143026b4b7519
Michael Zeitler <m.zeitler1987@gmail.com>
2026-05-25 17:03:41 +0200

    Animate about location icon and text

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function init() {
     setupActiveElements('.nav-list a');
     setupActiveElements('.language-btn');
     setupScrollDownButton();
+    setupAboutLocationAnimation();
 }
 
 function setupActiveElements(selector) {
@@ -31,6 +32,102 @@ function setupScrollDownButton() {
         resetActive(navLinks);
         setActive(aboutNavLink);
     });
+}
+
+function setupAboutLocationAnimation() {
+    let locationItems = [
+        {
+            icon: './assets/icons/location-web.svg',
+            text: 'I am based in Frankfurt.'
+        },
+        {
+            icon: './assets/icons/location-remote.svg',
+            text: 'I am open to remote work.'
+        }
+    ];
+
+    let aboutSection = document.getElementById('about');
+    let iconElement = document.getElementById('aboutLocationIcon');
+    let textElement = document.getElementById('aboutLocationText');
+
+    if (!aboutSection || !iconElement || !textElement) {
+        return;
+    }
+
+    iconElement.src = locationItems[0].icon;
+    textElement.textContent = '';
+
+    let observer = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+            startAboutLocationLoop(iconElement, textElement, locationItems);
+            observer.disconnect();
+        }
+    }, {
+        threshold: 0.4
+    });
+
+    observer.observe(aboutSection);
+}
+
+function startAboutLocationLoop(iconElement, textElement, locationItems) {
+    let currentIndex = 0;
+
+    animateAboutLocationItem(iconElement, textElement, locationItems, currentIndex);
+}
+
+function animateAboutLocationItem(iconElement, textElement, locationItems, currentIndex) {
+    let currentItem = locationItems[currentIndex];
+
+    iconElement.src = currentItem.icon;
+
+    typeText(textElement, currentItem.text, () => {
+        setTimeout(() => {
+            deleteText(textElement, () => {
+                let nextIndex = (currentIndex + 1) % locationItems.length;
+
+                iconElement.classList.add('is-hidden');
+
+                setTimeout(() => {
+                    iconElement.src = locationItems[nextIndex].icon;
+                    iconElement.classList.remove('is-hidden');
+
+                    animateAboutLocationItem(iconElement, textElement, locationItems, nextIndex);
+                }, 300);
+            });
+        }, 1200);
+    });
+}
+
+function typeText(element, text, callback) {
+    let index = 0;
+    element.textContent = '';
+
+    let typingInterval = setInterval(() => {
+        element.textContent += text[index];
+        index++;
+
+        if (index >= text.length) {
+            clearInterval(typingInterval);
+
+            if (callback) {
+                callback();
+            }
+        }
+    }, 70);
+}
+
+function deleteText(element, callback) {
+    let deletingInterval = setInterval(() => {
+        element.textContent = element.textContent.slice(0, -1);
+
+        if (element.textContent.length === 0) {
+            clearInterval(deletingInterval);
+
+            if (callback) {
+                callback();
+            }
+        }
+    }, 40);
 }
 
 function resetActive(elements) {
